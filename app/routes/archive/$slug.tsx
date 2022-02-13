1c4906a50b0405dee5cbc36381ea11673ed5ffa4
Son Thanh Vo <sonvo95@outlook.com>
2022-02-13 15:48:50 +0100

    chore: add error log of malformed uri

--- app/routes/archive/$slug.tsx
+++ app/routes/archive/$slug.tsx
@@ -1,10 +1,13 @@
 import { LoaderFunction, useLoaderData } from 'remix';
 
+import { Log } from '~/service/logger';
+
 interface Data {
   id: number;
 }
 
 export const loader: LoaderFunction = (args): Data => {
+  const logger = new Log('Archive Post');
   const { params } = args;
 
   const postId =
@@ -13,6 +16,7 @@ export const loader: LoaderFunction = (args): Data => {
       : Number.NaN;
 
   if (Number.isNaN(postId)) {
+    logger.error(`Request of resource with malformed [uri=${params.slug}]`);
     throw new Response('Invalid request', { status: 400 });
   }
 
