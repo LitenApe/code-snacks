26f4f2e25f42604c15b614d0bd19e1ac0b2e0bcd
Son Thanh Vo <sonvo95@outlook.com>
2022-03-06 14:23:38 +0100

    feat: add ArticleCard

---
+++ app/features/ArticleOverview/ArticleCard.tsx
@@ -0,0 +1,29 @@
+import { Heading } from '~/components/layout/Heading';
+import { Link } from 'remix';
+import { PostDTO } from '~/service/cms/domain';
+import { Routes } from '~/lib/routes';
+import { isDefined } from '~/lib/isDefined';
+
+interface Props {
+  readonly post: PostDTO;
+  readonly level?: 1 | 2 | 3 | 4 | 5 | 6;
+}
+
+export function ArticleCard(props: Props): JSX.Element {
+  const { post, level = 2 } = props;
+  const { id, title, publishedAt, createdAt } = post;
+  const isDraft = !isDefined(publishedAt);
+  const timestamp = isDraft ? createdAt : publishedAt;
+  const uri = isDraft ? Routes.DRAFTS : Routes.ARCHIVE;
+
+  return (
+    <Link to={`${uri}/${id}`}>
+      <article>
+        <Heading level={level}>{title}</Heading>
+        <p>
+          <time dateTime={timestamp}>{timestamp}</time>
+        </p>
+      </article>
+    </Link>
+  );
+}
--- app/features/ArticleOverview/ArticleOverview.tsx
+++ app/features/ArticleOverview/ArticleOverview.tsx
@@ -1,8 +1,5 @@
-import { Heading } from '~/components/layout/Heading';
-import { Link } from 'remix';
+import { ArticleCard } from './ArticleCard';
 import { PostDTO } from '~/service/cms/domain';
-import { Routes } from '~/lib/routes';
-import { isDefined } from '~/lib/isDefined';
 
 interface Props {
   readonly level?: 2 | 3 | 4 | 5 | 6;
@@ -14,25 +11,13 @@ export function ArticleOverview(props: Props): JSX.Element {
 
   return (
     <ul>
-      {posts.map((post): JSX.Element => {
-        const { id, title, publishedAt, createdAt } = post;
-        const isDraft = !isDefined(publishedAt);
-        const timestamp = isDraft ? createdAt : publishedAt;
-        const uri = isDraft ? Routes.DRAFTS : Routes.ARCHIVE;
-
-        return (
-          <li key={`article-overview-${isDraft}-${id}`}>
-            <Link to={`${uri}/${id}`}>
-              <article>
-                <Heading level={level}>{title}</Heading>
-                <p>
-                  <time dateTime={timestamp}>{timestamp}</time>
-                </p>
-              </article>
-            </Link>
+      {posts.map(
+        (post): JSX.Element => (
+          <li key={`article-overview-${post.id}`}>
+            <ArticleCard post={post} level={level} />
           </li>
-        );
-      })}
+        )
+      )}
     </ul>
   );
 }
