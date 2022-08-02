037d1e87b3dcdf59716f3c59866f17b3e7df54b6
Son Thanh Vo <sonvo95@outlook.com>
2022-08-02 16:15:22 +0200

    feat: add archive page

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,36 @@
+import { CMS, Frontmatter } from '~/services/cms';
+import { Link, useLoaderData } from '@remix-run/react';
+
+type Data = Array<Frontmatter>;
+
+export async function loader(): Promise<Data> {
+  const posts = await CMS.getPosts();
+  return posts;
+}
+
+export default function Index(): JSX.Element {
+  const data = useLoaderData<Data>();
+
+  return (
+    <>
+      <h1>Archive</h1>
+      <ul>
+        {data.map((post) => (
+          <li key={`/posts/${post.id}`}>
+            <Link to={`/posts/${post.id}`}>
+              <article>
+                <header>{post.frontmatter.title}</header>
+                <p>
+                  published:{' '}
+                  <time dateTime={post.frontmatter.date}>
+                    {new Date(post.frontmatter.date).toLocaleDateString()}
+                  </time>
+                </p>
+              </article>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
