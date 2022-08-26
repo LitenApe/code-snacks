869bf466da671727d4ea85af1a5ba3d76528b9d6
Son Thanh Vo <sonvo95@outlook.com>
2022-08-26 15:26:37 +0200

    chore: remove unused content source adapter

--- app/services/cms/sources/index.ts
+++ app/services/cms/sources/index.ts
@@ -1,4 +1,3 @@
-import { GitHub } from './github';
 import { Local } from './local';
 
-export const source = process.env.NODE_ENV === 'development' ? new Local() : new GitHub();
+export const source = new Local();
