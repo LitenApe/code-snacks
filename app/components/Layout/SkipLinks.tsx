d2870ac615728bd5d566fa78889493a1c302a6b8
Son Thanh Vo <sonvo95@outlook.com>
2022-09-27 14:37:01 +0200

    feat: add skip link focus

--- app/components/Layout/SkipLinks.tsx
+++ app/components/Layout/SkipLinks.tsx
@@ -1,8 +1,49 @@
+import {
+  useCallback, useEffect, useRef, useState,
+} from 'react';
+
 import { Landmarks } from './landmarks';
 
+enum NAVIGATION_MODE {
+  KEYBOARD,
+  MOUSE,
+}
+
 export function SkipLinks(): JSX.Element {
+  const containerRef = useRef<HTMLElement | null>(null);
+  const [mode, setMode] = useState<NAVIGATION_MODE | null>(null);
+
+  const updateNavigationMode = useCallback(
+    (event: MouseEvent | KeyboardEvent) => {
+      if (event.type === 'mousedown' && mode !== NAVIGATION_MODE.MOUSE) {
+        setMode(NAVIGATION_MODE.MOUSE);
+      } else if (
+        event.type === 'keydown'
+        && mode !== NAVIGATION_MODE.KEYBOARD
+      ) {
+        setMode(NAVIGATION_MODE.KEYBOARD);
+
+        // focus first element in skiplink list
+        if ((event as KeyboardEvent).key === 'Tab') {
+          containerRef.current?.focus();
+        }
+      }
+    },
+    [mode, setMode, containerRef.current],
+  );
+
+  useEffect(() => {
+    document.addEventListener('keydown', updateNavigationMode);
+    document.addEventListener('mousedown', updateNavigationMode);
+
+    return () => {
+      document.removeEventListener('keydown', updateNavigationMode);
+      document.removeEventListener('mousedown', updateNavigationMode);
+    };
+  }, [updateNavigationMode]);
+
   return (
-    <nav id="skip-links">
+    <nav ref={containerRef} id="skip-links" tabIndex={-1}>
       <ul>
         <li>
           <a href={`#${Landmarks.NAVBAR}`}>Navigation bar</a>
